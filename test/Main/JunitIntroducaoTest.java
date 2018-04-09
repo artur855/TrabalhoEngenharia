c8155da254bbe9b617e19207b3d8e0a5f2826d2c
artur855 <artur855@hotmail.com>
2018-04-09 10:03:28 -0300

    rs

--- test/Main/JunitIntroducaoTest.java
+++ test/Main/JunitIntroducaoTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Main;
 
 import java.util.ArrayList;
@@ -14,10 +9,6 @@ import model.VendaProduto;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author edmar
- */
 public class JunitIntroducaoTest {
 
     @Test
